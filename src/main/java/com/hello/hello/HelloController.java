701d08849a8aaf0396ecbd638014b767430c7fbf
masami-tr <m3o.mmmnnn@gmail.com>
2023-08-22 15:58:26 +0900

    Mapを追加しmessage,thank youを表示

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -3,12 +3,14 @@ package com.hello.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public Map<String, String> hello(){
+        return Map.of("message","thank you");
     }
 
 }
